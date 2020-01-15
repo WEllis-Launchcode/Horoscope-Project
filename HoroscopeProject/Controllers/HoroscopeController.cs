5aa0c8c7cb763bd3f63d942a20dc13a5f5501be9
Wesley Ellis <wesley@ellis.org>
2020-01-14 20:50:51 -0500

    Added Work

---
+++ HoroscopeProject/Controllers/HoroscopeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HoroscopeProject.Controllers
+{
+    public class HoroscopeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Select()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [Route("/Horoscope/Quote-Of-The-Day")]
+        public IActionResult Result()
+        {
+            return View();
+        }
+    }
+}
